4b5fd76b41a61d0e91f0f20d3ac4383ed84a89c0
MushexAvetisyan <avetisyan.mushex@inbox.ru>
2022-02-16 20:24:07 +0400

    start

--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {far} from "@fortawesome/free-regular-svg-icons";
+import {fas, faS} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+
+library.add(fas)
+library.add(far)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/NavBar.vue
+++ src/views/NavBar.vue
@@ -10,11 +10,23 @@
       </ul>
       <div class="searchNavbar">
         <input type="search" placeholder="I'm searching for..." />
-        <span>JOIN US</span>
-        <router-link to="/WishList"></router-link>
-        <h3>{{ productCount }}</h3>
+        <span class="join">JOIN US</span>
+        <router-link to="/WishList"
+          ><font-awesome-icon class="heart" icon="fa-regular fa-heart"
+        /></router-link>
+        <span @click="isFullSize = !isFullSize"
+          ><font-awesome-icon class="cart" icon="fa-solid fa-bag-shopping"
+        /></span>
+        <!--        <h3>{{ productCount }}</h3>-->
       </div>
     </nav>
+    <header v-if="isFullSize" class="header" role="banner" @click.stop="isFullSize = !isFullSize">
+      <div class="sidebar" @click.stop>
+        <span class="CloseSidebar" @click="isFullSize = !isFullSize">asdad</span>
+
+        <p>asaasdsdd</p>
+      </div>
+    </header>
   </div>
 </template>
 
@@ -22,6 +34,7 @@
 export default {
   name: "Home",
   data: () => ({
+    isFullSize: false,
     MenuItems: [
       { content: "home", router: "/HomeComponent", id: 1 },
       { content: "browse", router: "/Browse", id: 2 },
@@ -38,8 +51,47 @@ export default {
   display: flex;
   align-items: baseline;
 }
+
+.fullSize {
+  transition: 0.5s;
+  transform: translateX(700px);
+}
+
+.sidebar {
+  width: 500px;
+  transition: 0.5s;
+  position: fixed;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  background: #05081d 0 0 no-repeat padding-box;
+  opacity: 1;
+  .CloseSidebar {
+    color: white;
+  }
+}
+.header{
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: #55555550;
+}
+
+.mushex{
+  position: fixed;
+  width: 100%;
+  left: 0;
+  top: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+  height: 100%;
+  //display: none;
+}
 .searchNavbar {
   padding-left: 50px;
+  display: flex;
+  align-items: center;
   input {
     height: 45px;
     width: 600px;
@@ -51,7 +103,7 @@ export default {
     outline: none;
     cursor: pointer;
   }
-  span {
+  .join {
     background-color: #383f49;
     color: white !important;
     padding: 13px 20px;
@@ -59,6 +111,13 @@ export default {
     text-decoration-line: none !important;
     font-size: 16px !important;
     margin-left: 50px;
+  }
+  .heart,
+  .cart {
+    width: 45px;
+    height: 35px;
+    margin-left: 15px;
+    cursor: pointer;
   }
 }
 </style>
